9d1ef515c528d895e99799a41aedd65c0bf9ddf5
sebasgaspar99 <sebastian.gaspar@onetree.com>
2022-07-15 22:56:36 -0500

    Add responsive in page single-property

--- src/Components/DescriptionProperty/DescriptionProperty.component.tsx
+++ src/Components/DescriptionProperty/DescriptionProperty.component.tsx
@@ -1,83 +1,95 @@
-import { useEffect, useState } from 'react'
-import { PriceStyle } from '../../Atoms'
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { PriceStyle } from "../../Atoms";
 
-import { PropertyModel } from '../../models/Property.model'
-import { DescriptionItem, DetailsItem } from '../../Interfaces'
+import { PropertyModel } from "../../models/Property.model";
+import { DescriptionItem, DetailsItem } from "../../Interfaces";
 
-import { DescriptionItems } from './DescriptionItems/DescriptionItems.component'
-import { DetailsProperty } from './DetailsProperty/DetailsProperty.component'
-import { FeaturesProperty } from './FeaturesProperty/FeaturesProperty.component'
-import { InterestProperty } from './InterestProperty/InterestProperty.component'
+import { DescriptionItems } from "./DescriptionItems/DescriptionItems.component";
+import { MobileBanner } from "./MobileBanner/MobileBanner.component";
+import { DetailsProperty } from "./DetailsProperty/DetailsProperty.component";
+import { FeaturesProperty } from "./FeaturesProperty/FeaturesProperty.component";
+import { InterestProperty } from "./InterestProperty/InterestProperty.component";
 
-import Location from '../../Assets/Icons/Common/Ubicacion.svg'
+import Location from "../../Assets/Icons/Common/Ubicacion.svg";
 
-import './style.css'
+import "./style.css";
 
-export function DescriptionPropertyComponent(props: PropertyModel): JSX.Element {
-    const property = props
+export function DescriptionPropertyComponent(
+  props: PropertyModel
+): JSX.Element {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 850px)",
+  });
+  const property = props;
 
-    const [descriptionItem, setDescriptionItem] = useState<DescriptionItem>();
-    const [detailsItem, setDetailsItem] = useState<DetailsItem>();
-    const [interestItem, setInterestItem] = useState<object[]>();
+  const [descriptionItem, setDescriptionItem] = useState<DescriptionItem>();
+  const [detailsItem, setDetailsItem] = useState<DetailsItem>();
+  const [interestItem, setInterestItem] = useState<object[]>();
 
-    useEffect((): void => {
-        if (property) {
-            setDescriptionItem(
-                {
-                    bathroom: property.PropertyDetails.bathrooms,
-                    parking: property.parking,
-                    private_area: property.PropertyDetails.area,
-                    antiquity: property.PropertyDetails.antiquity,
-                    stratum: property.PropertyDetails.stratum,
-                    admin_price: property.PropertyDetails.admin,
-                }
-            );
-            setDetailsItem(
-                {
-                    canon_price: property.PropertyDetails.canon_price,
-                    admin: property.PropertyDetails.admin,
-                    address: property.PropertyDetails.address,
-                    street: property.PropertyDetails.street,
-                    type: property.PropertyDetails.type,
-                    antiquity: property.PropertyDetails.antiquity,
-                    floor: property.PropertyDetails.floor,
-                    area: property.PropertyDetails.area,
-                    garage: property.PropertyDetails.garage,
-                    bathrooms: property.PropertyDetails.bathrooms,
-                    stratum: property.PropertyDetails.stratum,
-                    elevator: property.PropertyDetails.elevator,
-                }
-            );
-            setInterestItem(property.interest)
-        }
-    }, [property]);
-    return (
-        <>
-            {
-                property
-                    ? <div className="single-description-property p-4 px-8 font-lato">
-                        < h2 className="tittle-description" > {property.name}</h2 >
-                        <label className='cod-description' >{property.code}</label>
-                        <p className='price-description text-greenCyan'>
-                            <PriceStyle number={property.PropertyDetails.canon_price} />
-                        </p>
-                        <div className='flex'>
-                            <img src={Location} alt="" style={{ marginRight: '0.5rem' }} width={22} />
-                            <label className='city-description'>{property.city}</label>
-                        </div>
-                        <DescriptionItems {...descriptionItem} />
-                        <hr />
-                        <h2 className='subtittle-description'>Descripción General</h2>
-                        <p className='paragraph-description'>{property.description}</p>
-                        <hr />
-                        <DetailsProperty {...detailsItem} />
-                        <hr />
-                        <FeaturesProperty {...property.features} />
-                        <hr />
-                        <InterestProperty {...interestItem} />
-                    </div >
-                    : null
-            }
-        </>
-    )
+  useEffect((): void => {
+    if (property) {
+      setDescriptionItem({
+        bathroom: property.PropertyDetails.bathrooms,
+        parking: property.parking,
+        private_area: property.PropertyDetails.area,
+        antiquity: property.PropertyDetails.antiquity,
+        stratum: property.PropertyDetails.stratum,
+        admin_price: property.PropertyDetails.admin,
+      });
+      setDetailsItem({
+        canon_price: property.PropertyDetails.canon_price,
+        admin: property.PropertyDetails.admin,
+        address: property.PropertyDetails.address,
+        street: property.PropertyDetails.street,
+        type: property.PropertyDetails.type,
+        antiquity: property.PropertyDetails.antiquity,
+        floor: property.PropertyDetails.floor,
+        area: property.PropertyDetails.area,
+        garage: property.PropertyDetails.garage,
+        bathrooms: property.PropertyDetails.bathrooms,
+        stratum: property.PropertyDetails.stratum,
+        elevator: property.PropertyDetails.elevator,
+      });
+      setInterestItem(property.interest);
+    }
+  }, [property]);
+  return (
+    <>
+      {property ? (
+        <div className="single-description-property p-4 px-8 font-lato">
+          <h2 className="tittle-description"> {property.name}</h2>
+          <label className="cod-description">{property.code}</label>
+          <p className="price-description text-greenCyan">
+            <PriceStyle number={property.PropertyDetails.canon_price} />
+          </p>
+          <div className="flex">
+            <img
+              src={Location}
+              alt=""
+              style={{ marginRight: "0.5rem" }}
+              width={22}
+            />
+            <label className="city-description">{property.city}</label>
+          </div>
+          <DescriptionItems {...descriptionItem} />
+          {isMobile ? (
+            <>
+              <hr />
+              <MobileBanner />
+            </>
+          ) : null}
+          <hr />
+          <h2 className="subtittle-description">Descripción General</h2>
+          <p className="paragraph-description">{property.description}</p>
+          <hr />
+          <DetailsProperty {...detailsItem} />
+          <hr />
+          <FeaturesProperty {...property.features} />
+          <hr />
+          <InterestProperty {...interestItem} />
+        </div>
+      ) : null}
+    </>
+  );
 }
--- src/Components/PropertyImageCarousel/PropertyImageCarousel.component.tsx
+++ src/Components/PropertyImageCarousel/PropertyImageCarousel.component.tsx
@@ -1,122 +1,152 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import Slider from "react-slick"
+import Slider from "react-slick";
 
-import Vr from '../../Assets/Icons/Common/VrIcon.svg';
-import { ImPlay2 } from 'react-icons/im';
-import { IoMdImage } from 'react-icons/io';
-import { MdOutline360 } from 'react-icons/md'
+import Vr from "../../Assets/Icons/Common/VrIcon.svg";
+import { useMediaQuery } from "react-responsive";
+import { ImPlay2 } from "react-icons/im";
+import { IoMdImage } from "react-icons/io";
+import { MdOutline360 } from "react-icons/md";
 import { PropertyModel } from "../../models/Property.model";
 
 import { useParams } from "react-router-dom";
 import { PropertyService } from "../../Services/Property.service";
 
-import './style.css';
+import "./style.css";
 
 type imgObject = {
-    link: string,
-}
-export function PropertyImageCarouselComponent(props: PropertyModel): JSX.Element {
-    const propertyService = new PropertyService();
-    const [images, setImages] = useState<imgObject[]>([]);
-    const { id } = useParams();
+  link: string;
+};
+export function PropertyImageCarouselComponent(
+  props: PropertyModel
+): JSX.Element {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 850px)",
+  });
 
-    useEffect((): void => {
-        if (id) _getImageProperty(+id);
-    }, [id]);
-    const _getImageProperty = async (id: number) => {
-        const propertiyResponse: imgObject[] = await propertyService.getPropertyImage(id);
-        if (propertiyResponse) setImages(propertiyResponse);
-    }
-    const customeSlider = React.createRef<any>();
-    const changeSizeFirstImg = () => {
-        const slider = document.querySelector('.slick-current')!;
-        if (slider) {
-            const firstImg = slider.querySelector('.img-container-carousel')!;
-            firstImg.classList.add('big-img');
-        }
-    }
-    const changeOtherSizeImg = () => {
-        const otherImg = document.querySelectorAll('.img-container-carousel');
-        otherImg.forEach((item: Element) => {
-            item.classList.remove('big-img');
-        });
-        changeSizeFirstImg();
-    }
-    const [sliderSettings, setSliderSettings] = useState({
-        dots: false,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        speed: 1200,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true,
-        beforeChange: changeSizeFirstImg,
-        afterChange: changeOtherSizeImg,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
-    })
-    useEffect(() => {
-        changeOtherSizeImg();
-    }, [images])
+  const propertyService = new PropertyService();
 
-    const _openLink = (link: string) => {
-        window.open(link);
+  const [images, setImages] = useState<imgObject[]>([]);
+  const { id } = useParams();
+
+  const changeSizeFirstImg = () => {
+    const slider = document.querySelector(".slick-current")!;
+    if (slider) {
+      const firstImg = slider.querySelector(".img-container-carousel")!;
+      firstImg.classList.add("big-img");
     }
-    return (
-        <div id="property-single-carousel" className='property-image-carousel'>
-            <div className='slider-image-carousel py-4' style={{ width: '95%', margin: 'auto' }}>
-                {images.length > 0
-                    ? <Slider ref={customeSlider} {...sliderSettings}>
-                        {images.map((item, index) => {
-                            return <div key={index}>{ImageCarousel({ img: item.link })}</div>
-                        })}
-                    </Slider>
-                    : null
-                }
-            </div>
-            <div className="btn-interaction-container flex">
-                <div className="card-interaction" onClick={() => _openLink(props.link_360)}>
-                    <MdOutline360 style={{ fontSize: '2.2rem' }} />
-                </div>
-                <div className="card-interaction">
-                    <ImPlay2 style={{ fontSize: '2rem' }} />
-                </div>
-                <div className="card-interaction">
-                    <IoMdImage style={{ fontSize: '2rem' }} />
-                </div>
-                <div className="card-interaction">
-                    <img src={Vr} alt="" width={60} />
-                </div>
-            </div>
+  };
+  const changeOtherSizeImg = () => {
+    const otherImg = document.querySelectorAll(".img-container-carousel");
+    otherImg.forEach((item: Element) => {
+      item.classList.remove("big-img");
+    });
+    changeSizeFirstImg();
+  };
+  const [sliderSettings, setSliderSettings] = useState({
+    dots: false,
+    infinite: true,
+    autoplay: isMobile ? false : true,
+    autoplaySpeed: 5000,
+    speed: 1200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: isMobile ? false : true,
+    beforeChange: isMobile ? () => {} : changeSizeFirstImg,
+    afterChange: isMobile ? () => {} : changeOtherSizeImg,
+    nextArrow: isMobile ? <></> : <SampleNextArrow />,
+    prevArrow: isMobile ? <></> : <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  });
+  useEffect(() => {
+    if (!isMobile) changeOtherSizeImg();
+  }, [images]);
+  useEffect((): void => {
+    if (id) _getImageProperty(+id);
+  }, []);
+  const _getImageProperty = async (id: number) => {
+    const propertiyResponse: imgObject[] =
+      await propertyService.getPropertyImage(id);
+    if (propertiyResponse) setImages(propertiyResponse);
+  };
+  const customeSlider = React.createRef<any>();
+
+  const _openLink = (link: string) => {
+    window.open(link);
+  };
+  return (
+    <div id="property-single-carousel" className="property-image-carousel">
+      <div className="slider-image-carousel py-4">
+        {images.length > 0 ? (
+          <Slider ref={customeSlider} {...sliderSettings}>
+            {images.map((item, index) => {
+              return <div key={index}>{ImageCarousel({ img: item.link })}</div>;
+            })}
+          </Slider>
+        ) : null}
+      </div>
+      <div className="btn-interaction-container flex">
+        <div
+          className="card-interaction"
+          onClick={() => _openLink(props.link_360)}
+        >
+          <MdOutline360 style={{ fontSize: isMobile ? "1.8rem" : "2.2rem" }} />
         </div>
-    )
+        <div className="card-interaction">
+          <ImPlay2 style={{ fontSize: isMobile ? "1.5rem" : "2rem" }} />
+        </div>
+        <div className="card-interaction">
+          <IoMdImage style={{ fontSize: isMobile ? "1.5rem" : "2rem" }} />
+        </div>
+        <div className="card-interaction">
+          <img src={Vr} alt="" width={isMobile ? 40 : 60} />
+        </div>
+      </div>
+    </div>
+  );
 }
 function SamplePrevArrow(props: any) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block" }}
-            onClick={onClick}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
 }
 
 function SampleNextArrow(props: any) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block" }}
-            onClick={onClick}
-        />
-    );
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
 }
 function ImageCarousel({ img }: any): JSX.Element {
-    return (
-        <div className="img-container-carousel">
-            <img src={img} alt="img_test" />
-        </div>
-    )
+  return (
+    <div className="img-container-carousel">
+      <img src={img} alt="img_test" />
+    </div>
+  );
 }
