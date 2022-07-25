97e482e3080f9a1c7a301f44418ed87d2ea55cc1
Oscar Alonso <oscar.alsa11@gmail.com>
2022-07-25 06:57:22 -0500

    Header responsive

--- src/Components/Header/Header.component.tsx
+++ src/Components/Header/Header.component.tsx
@@ -9,22 +9,24 @@ export function HeaderComponent(): JSX.Element {
     let headerInfo: SimpleData = HeaderInfo;
 
     return (
-        <div className="container-header grid grid-cols-2">
-            <div className="flex self-center flex-col">
-                <h1 className="text-pursianBlue font-lato font-bold w-4/5 block mx-auto">
-                    {headerInfo.text}
-                </h1>
+        <>
+            <div className="container-header grid grid-cols-2">
+                <div className="flex self-center flex-col">
+                    <h1 className="text-pursianBlue font-lato block text-class">
+                        {headerInfo.text}
+                    </h1>
+                </div>
+                <div className='img-header-container self-center'>
+                    <img id='img-header' src={ImageExample} alt="Header img" />
+                    <img id='logo-header-back' src={ImageExample} alt="Header img" />
+                </div>
             </div>
-            <div className='img-header-container py-4'>
-                <img id='img-header' src={ImageExample} alt="Header img" />
-                <img id='logo-header-back' src={ImageExample} alt="Header img" />
-            </div>
+
             <div className='relative top-0 left-0'>
                 <div className='filter-container'>
                     <HeaderFilterProperty />
                 </div>
             </div>
-
-        </div>
+        </>
     );
 }
